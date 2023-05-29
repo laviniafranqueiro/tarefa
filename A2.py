c2f0827a83bb8d09842b476fce40e1bad746e14b
laviniafranqueiro <133999323+laviniafranqueiro@users.noreply.github.com>
2023-05-29 18:23:24 -0300

    Create A2.py

---
+++ A2.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd 
+import csv
+st.title('tarefa do Josir')
+st.caption('Lavínia Franqueiro')
+
+df = pd.read_csv('acidentes.csv', sep =';')
+st.dataframe(df)
+
+#arquivo = open('acidentes.csv')
+#for linha in arquivo:
+#    st.write(linha)
