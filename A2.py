a47ecacffc4524acae1e5ca8558eace82904da21
laviniafranqueiro <133999323+laviniafranqueiro@users.noreply.github.com>
2023-05-30 08:48:11 -0300

    Update A2.py

--- A2.py
+++ A2.py
@@ -4,9 +4,9 @@ import csv
 st.title('avaliação do Josir')
 st.caption('Lavínia Franqueiro')
 
-df = pd.read_csv('preço_notebooks_excel.xlsx', sep =';')
+df = pd.read_csv('preço_notebooks.csv', sep =';')
 st.dataframe(df)
 
-#arquivo = open('preço_notebooks_excel.xlsx')
+#arquivo = open('preço_notebooks.csv')
 #for linha in arquivo:
 #    st.write(linha)
