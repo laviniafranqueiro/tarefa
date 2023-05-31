5beae7abc72b3d30086bf07dfb45b63c3c16df95
laviniafranqueiro <133999323+laviniafranqueiro@users.noreply.github.com>
2023-05-31 16:46:53 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd 
-import csv
 st.title('tarefa do Josir')
 st.caption('Lavínia Franqueiro')
 
@@ -8,8 +7,6 @@ df = pd.read_csv('acidentes.csv', sep =',')
 st.dataframe(df)
 
 chart_data = df[['Road Accidents during 2018', 'Road Accidents during 2019', 'Road Accidents during 2020']]
-st.bar_chart(chart_data)
+st.line_chart(chart_data)
 
-#arquivo = open('acidentes.csv')
-#for linha in arquivo:
-#    st.write(linha)
+
