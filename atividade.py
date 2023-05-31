75250b28186626504a2cc907d3d02dc18592a8cc
laviniafranqueiro <133999323+laviniafranqueiro@users.noreply.github.com>
2023-05-31 16:40:41 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -7,6 +7,9 @@ st.caption('Lavínia Franqueiro')
 df = pd.read_csv('acidentes.csv', sep =',')
 st.dataframe(df)
 
+chart_data = df[['Road Accidents 2018 ', 'Road Accidents 2019', 'Road Accidents 2020 ']]
+st.bar_chart(chart_data)
+
 #arquivo = open('acidentes.csv')
 #for linha in arquivo:
 #    st.write(linha)
