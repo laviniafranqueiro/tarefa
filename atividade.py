6d9bbc7f95d108f3778e71332bce697155516b3d
laviniafranqueiro <133999323+laviniafranqueiro@users.noreply.github.com>
2023-06-02 09:12:41 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -6,8 +6,3 @@ st.caption('Lavínia Franqueiro')
 
 df = pd.read_csv('acidentes.csv', sep =',')
 st.dataframe(df)
-
-chart_data = df[['Road Accidents during 2018', 'Road Accidents during 2019', 'Road Accidents during 2020']]
-st.line_chart(chart_data)
-
-
